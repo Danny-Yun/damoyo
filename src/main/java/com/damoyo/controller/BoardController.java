12c3447ca0895c79d418560f8924a31c1fd2161f
hanbeen03 <hanbeen03@hanmail.net>
2021-09-25 16:31:37 +0900

    20210925 4:31

--- src/main/java/com/damoyo/controller/BoardController.java
+++ src/main/java/com/damoyo/controller/BoardController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.damoyo.domain.BoardVO;
 import com.damoyo.service.BoardService;
@@ -30,4 +32,29 @@ public class BoardController {
 		
 		model.addAttribute("list", boards);
 	}
+	
+	@GetMapping("/detail")
+	public void detail(Model model, Long b_num) {
+		
+		BoardVO vo = service.getBoard(b_num);
+		
+		model.addAttribute("vo", vo);
+	}
+	
+	@PostMapping("/delete")
+	public String delete(Long b_num, RedirectAttributes rttr) {
+		
+		service.delete(b_num);
+		
+		rttr.addFlashAttribute("delete", "success");
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/write")
+	public void write() {
+		
+	}
+	
+	
 }
