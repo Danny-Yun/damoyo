27087816904415ac336a7360e925f8a7952b4463
DESKTOP-UV9647E\fbrk0 <wakebro119@naver.com>
2021-10-16 18:44:23 +0900

    20211016_모임 프로필 사진 업데이트

--- src/main/java/com/damoyo/controller/MainController.java
+++ src/main/java/com/damoyo/controller/MainController.java
@@ -1,15 +1,22 @@
 package com.damoyo.controller;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.damoyo.domain.InterestVO;
@@ -33,6 +40,17 @@ public class MainController {
 	@Autowired
 	private MainService service;
 	
+	private String getFolder() {
+		// 날짜 포맷
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		// 오늘 날짜 객체 생성
+		Date date = new Date();
+		// 설정한 날짜 포맷에 날짜 객체를 넣은 후 String으로 저장
+		String str = sdf.format(date);
+		// 문자열로 저장한 날짜 포맷의 '-'을 OS 규격에 맞추어 재조정 후 리턴
+		return str.replace("-", File.separator);
+	}
+	
 	@GetMapping("/")
 	public String list(Model model, HttpSession session, MainSearchCriteria cri) {
 		String u_id = (String) session.getAttribute("u_id");
@@ -50,9 +68,9 @@ public class MainController {
 		if(cri.getSearchType() == null)
 			cri.setSearchType("");
 		int total = service.getTotalMeet(cri);
-		log.info("컨트롤러");
-		log.info(cri.getSearchType());
-		log.info(total);
+//		log.info("컨트롤러");
+//		log.info(cri.getSearchType());
+//		log.info(total);
 		List<InterestVO> interestList = service.get();
 		List<MeetVO> meetList = service.getListMeet(cri);
 		MainPageDTO meetPages = new MainPageDTO(total, cri);
@@ -78,20 +96,40 @@ public class MainController {
 		return "/main/register";
 	}
 	@PostMapping("/register")
-	public String register(RedirectAttributes rttr, MeetVO vo) {
+	public String register(RedirectAttributes rttr, MeetVO vo, @Param("m_profile")MultipartFile profile) {
 		// 모임 생성
-		service.registerMeet(vo);
+		log.info(profile);
+		if(profile.getSize() != 0) {
+			log.info("모임 생성 사진 이름 : " + profile.getOriginalFilename());
+			log.info("모임 생성 사진 크기 : " + profile.getSize());
+			
+			String uploadFolder = "C:\\upload_data\\temp\\meet_profile";
+			String uploadFolderPath = getFolder();
+			File uploadPath = new File(uploadFolder, uploadFolderPath);
+			
+			log.info("파일 저장 위치 : " + uploadPath);
+			
+			if(uploadPath.exists() == false)
+				uploadPath.mkdirs();
+			
+			String fileName = profile.getOriginalFilename();
+			fileName = fileName.substring(fileName.lastIndexOf("\\" + 1));
+			
+			UUID uuid = UUID.randomUUID();
+		}
+		
+//		service.registerMeet(vo);
 		// 모임 개설자 모임 회원 리스트 등록
 		MeetMemberVO member = new MeetMemberVO();
 		member.setM_num(vo.getM_num());
 		member.setU_id(vo.getU_id());
 		member.setMember_list_position("모임장");
-		service.joinMeet(member);
+//		service.joinMeet(member);
 		// 내가 가입한 모임에도 추가
 		MyJoinMeetVO myMeet = new MyJoinMeetVO();
 		myMeet.setU_id(vo.getU_id());
 		myMeet.setM_num(vo.getM_num());
-		service.saveMyJoinMeet(myMeet);
+//		service.saveMyJoinMeet(myMeet);
 		return "redirect:/main/";
 	}
 	
